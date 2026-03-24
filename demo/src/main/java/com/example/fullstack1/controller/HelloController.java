4d52dc4178fdc3d1601f5d21277e37eac70a787e
Satwik Saxena <satwiksaxena41@gmail.com>
2026-03-24 23:09:36 +0530

    asmr

--- demo/src/main/java/com/example/fullstack1/controller/HelloController.java
+++ demo/src/main/java/com/example/fullstack1/controller/HelloController.java
@@ -65,14 +65,12 @@ public class HelloController {
         double result = Math.log(number) / Math.log(base);
         return new Response(number,base,result,"Logrithmitic Calculation Successful");
     }
-
     @GetMapping("/expo")
     public squareResponse expo(@RequestParam double a) {
         double e = 2.71828182846;
         double res = e * a;
         return new squareResponse(a, res, "Exponential Succesfull");
     }
-
     @GetMapping("/fact")
     public squareResponse factorial(@RequestParam double a){
         long f=1;
@@ -82,27 +80,43 @@ public class HelloController {
         long res = f;
         return new squareResponse(a,res,"Factorial Successful");
     }
-
     @GetMapping("/sin")
     public TrigoResponse sinfunc(@RequestParam double a){
-
         // Convert degrees to radians
         double radians = Math.toRadians(a);
-
         double result = Math.sin(radians);
         String msg = "Sin("+a+") Succesful";
         return new TrigoResponse(a,result,msg);
     }
-
     @GetMapping("/cos")
     public TrigoResponse cosfunc(@RequestParam double a){
         double radians = Math.toRadians(a);
-
         double result = Math.cos(radians);
         if (Math.abs(result) < 1e-10) {
             result = 0;
         }
         String msg = "Cos("+ a+") Succesful";
         return new TrigoResponse(a,result,msg);
+    }
+    @GetMapping("/tan")
+    public TrigoResponse tanfunc(@RequestParam double a){
+        double radians = Math.toRadians(a);
+        double result = Math.tan(a);
+        if (Math.abs(result) < 1e-10) {
+            result = 0;
+        }
+        String msg = "tan("+a+") Succesful";
+        return new TrigoResponse(a,result,msg);
+    }
+    @GetMapping("/sec")
+    public TrigoResponse secfunc(@RequestParam double a){
+        double radians = Math.toRadians(a);
+        double cosValue = Math.cos(radians);
+        if (Math.abs(cosValue) < 1e-10) {
+            return new TrigoResponse(a, Double.NaN, "sec(" + a + ") Succesful");
+        }
+        double result = 1 / cosValue;
+        String msg = "sec(" + a + ") Successful";
+        return new TrigoResponse(a, result, msg);
     }
 }
